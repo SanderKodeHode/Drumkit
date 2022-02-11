02273c0ca62d4eb67eaf08bd9b2b60b77c2b2a78
SanderKodeHode <96051916+SanderKodeHode@users.noreply.github.com>
2022-02-11 09:54:39 +0100

    Update drumkit.js

--- drumkit.js
+++ drumkit.js
@@ -1,29 +1,25 @@
-console.log("yutfy")
-
 const drumKit = document.querySelector(".drum-kit");
-// events
+// EventListener
 document.addEventListener('keydown', keyInstrument);
 drumKit.addEventListener("mousedown", clickInstrument);
 
 
 const drumInstrument = {
-    clap: new Audio("./sounds/clap.wav"),
-    hihat: new Audio("./sounds/hihat.wav"),
     kick: new Audio("./sounds/kick.wav"),
     openhat: new Audio("./sounds/openhat.wav"),
     ride: new Audio("./sounds/ride.wav"),
     snare: new Audio("./sounds/snare.wav"),
     tink: new Audio("./sounds/tink.wav"),
-    tom: new Audio("./sounds/tom.wav")
+    tom: new Audio("./sounds/tom.wav"),
+    clap: new Audio("./sounds/clap.wav"),   
+    hihat: new Audio("./sounds/hihat.wav")
 }
-console.log(drumInstrument)
 
 
 //Numpad
 function keyInstrument(event) {
-    //Med If statements
-    const keyName = event.key; // 1,2,3,4,5...
-    console.log(event.key)
+//With If statements
+    const keyName = event.key;
     if (keyName == 1) drumInstrument.kick.play()
     if (keyName == 2) drumInstrument.openhat.play()
     if (keyName == 3) drumInstrument.ride.play()
@@ -33,18 +29,18 @@ function keyInstrument(event) {
     if (keyName == 7) drumInstrument.clap.play()
     if (keyName == 8) drumInstrument.hihat.play()
 
-//Med soundMap
-    const soundMap = {
-        1: drumInstrument.kick,
-        2: drumInstrument.openhat,
-        3: drumInstrument.ride,
-        4: drumInstrument.snare,
-        5: drumInstrument.tink,
-        6: drumInstrument.tom,
-        7: drumInstrument.clap,
-        8: drumInstrument.hihat,
-    }
-    soundMap[keynameName].play()
+//Alternative
+    // const soundMap = {
+    //     1: drumInstrument.kick,
+    //     2: drumInstrument.openhat,
+    //     3: drumInstrument.ride,
+    //     4: drumInstrument.snare,
+    //     5: drumInstrument.tink,
+    //     6: drumInstrument.tom,
+    //     7: drumInstrument.clap,
+    //     8: drumInstrument.hihat,
+    // }
+    // soundMap[keynameName].play()
 }
 
 //MouseClick
